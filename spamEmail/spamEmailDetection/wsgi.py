f0e03c0369f83949cdb0c9c52656e2ff5b2bf943
IAMPETER-CPU <ucpeter1999@gmail.com>
2026-01-31 22:03:53 -0800

    load

--- spamEmail/spamEmailDetection/wsgi.py
+++ spamEmail/spamEmailDetection/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spamEmailDetection.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spamEmail.spamEmailDetection.settings')
 
 application = get_wsgi_application()
